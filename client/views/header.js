8a81e63a0052c63876b4bdff657c05be06430f42
kevinweber <k-iwi@web.de>
2016-06-19 17:38:21 -0700

    Add responsive toggle navigation

--- client/views/header.js
+++ client/views/header.js
@@ -1,11 +1,26 @@
-Template.header.events({
-  'click [data-id=sign-out]': function() {
-    Meteor.logout(function(error) {
-      if (error) {
-        alert(error.reason);
-      } else {
-        FlowRouter.go('/sign-in');
-      }
+Template.navigation.onRendered(function () {
+  this.autorun(() => {
+    // Init navigation using responsive-nav.min.js
+    Meteor.navigation = responsiveNav(".nav-collapse", {
+      animate: true, // Boolean: Use CSS3 transitions, true or false
+      transition: 284, // Integer: Speed of the transition, in milliseconds
+      label: "Menu", // String: Label for the navigation toggle
+      insert: "after", // String: Insert the toggle before or after the navigation
+      customToggle: "#nav-toggle", // Selector: Specify the ID of a custom toggle
+      closeOnNavClick: false, // Boolean: Close the navigation when one of the links are clicked
+      openPos: "relative", // String: Position of the opened nav, relative or static
+      navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
+      navActiveClass: "js-nav-active", // String: Class that is added to <html> element when nav is active
+      jsClass: "js", // String: 'JS enabled' class which is added to <html> element
+      init: function () {}, // Function: Init callback
+      open: function () {}, // Function: Open callback
+      close: function () {} // Function: Close callback
     });
+  });
+});
+
+Template.navigation.events({
+  'click a': function () {
+    Meteor.navigation.close();
   }
 });
--- client/views/shared/_navigation.js
+++ client/views/shared/_navigation.js
@@ -30,3 +30,15 @@ Template.navigation.helpers({
     }
   }
 });
+
+Template.navigation.events({
+  'click [data-id=sign-out]': function() {
+    Meteor.logout(function(error) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        FlowRouter.go('/sign-in');
+      }
+    });
+  }
+});
