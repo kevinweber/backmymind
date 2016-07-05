1634749accee6868b170e993b2d55de6fdd7703c
kevinweber <k-iwi@web.de>
2016-07-04 19:52:05 -0700

    Make relations filterable

--- client/views/users/browseRelations.js
+++ client/views/users/browseRelations.js
@@ -1,40 +1,72 @@
-Template.browseRelations.events({
-  'click [data-id=load-more]': (event, template) => {
-    template.limit.set(template.limit.get() + 20);
-  },
+/*
+ * Test if specific relation matches filter
+ */
+function isValidRelation(object, filter) {
+  var filterableFields = ['email', 'firstName', 'lastName'];
 
+  for (let i = 0, l = filterableFields.length; i < l; i += 1) {
+    let field = object[filterableFields[i]];
+    if (field && field.indexOf(filter) !== -1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/*
+ * Filter an object with relations,
+ * and only return relations matching the filter
+ */
+function filterRelations(object, filter) {
+  var filtered = [];
+
+  for (let relation in object) {
+    if (object.hasOwnProperty(relation)) {
+      if (isValidRelation(object[relation], filter)) {
+        filtered.push(object[relation]);
+      }
+    }
+  }
+
+  return filtered;
+}
+
+
+Template.browseRelations.events({
   'keyup [data-id=search-query]': _.debounce((event, template) => {
     event.preventDefault();
     template.searchQuery.set(template.find('[data-id=search-query]').value);
-    template.limit.set(20);
-  }, 300),
-
-  'submit [data-id=search-users-form]': (event, template) => {
-    event.preventDefault();
-  }
+  }, 300)
 });
 
 Template.browseRelations.helpers({
   relations: () => {
+    const instance = Template.instance();
+    let filter = instance.searchQuery.get();
+
+    // If filter is given...
+    if (filter) {
+      let relations = Meteor.users.findOne({
+        _id: Meteor.userId()
+      }, {
+        fields: {
+          "relations": 1
+        }
+      }).relations;
+
+      relations = filterRelations(relations, filter);
+
+      return relations;
+    }
+
+    // Default: Display all relations
     return Meteor.user() && Meteor.user().relations;
-  },
-
-  users: () => {
-    return Meteor.users.find({ _id: { $ne: Meteor.userId() } }, { sort: { createdAt: -1 } });
-  },
-
-  hasMoreUsers: () => {
-    return Template.instance().limit.get() <= Template.instance().usersCount.get();
   }
 });
 
-Template.browseRelations.onCreated(function() {
+Template.browseRelations.onCreated(function () {
   this.searchQuery = new ReactiveVar('');
-  this.limit = new ReactiveVar(20);
-  this.usersCount = new ReactiveVar(0);
-
-  this.autorun(() => {
-    this.subscribe('users.all', this.searchQuery.get(), this.limit.get());
-    this.usersCount.set(Counts.get('users.all'));
-  });
+  
+  Meteor.subscribe('user.relations');
 });
--- server/publications.js
+++ server/publications.js
@@ -4,57 +4,8 @@ Meteor.publish(null, function() {
   }
 });
 
-Meteor.publish('userStatus', function() {
-  return Meteor.users.find({'status.online': true });
-});
-
-Meteor.publishComposite('posts.all', function(query, filter, limit) {
-  check(query, String);
-  check(filter, String);
-  check(limit, Number);
-
-  if (this.userId) {
-    let currentUser = Meteor.users.findOne({ _id: this.userId });
-
-    let parameters = {
-      find: {},
-      options: {}
-    };
-
-    if (filter === 'following') {
-      if (currentUser.followingIds && currentUser.followingIds.length !== 0) {
-        parameters.find.authorId = { $in: currentUser.followingIds };
-      } else {
-        parameters.find.authorId = { $in: [] };
-      }
-    }
-
-    return {
-      find: () => {
-        if (query) {
-          parameters.find.$text = { $search: query };
-          parameters.options = {
-            fields: { score: { $meta: 'textScore' } },
-            sort: { score: { $meta: 'textScore' } },
-            limit: limit
-          };
-        } else {
-          parameters.options = { sort: { createdAt: -1 }, limit: limit };
-        }
-        Counts.publish(this, 'posts.all', Posts.find(parameters.find), { noReady: true });
-        return Posts.find(parameters.find, parameters.options);
-      },
-      children: [
-        {
-          find: (post) => {
-            return Meteor.users.find({ _id: post.authorId }, { fields: { emails: 1, username: 1 } });
-          }
-        }
-      ]
-    };
-  } else {
-    return [];
-  }
+Meteor.publish('user.relations', function(query, limit) {
+  return Meteor.users.find({ _id: this.userId }, { fields: { relations: 1 } });
 });
 
 Meteor.publishComposite('users.profile', function(_id, limit) {
@@ -80,89 +31,142 @@ Meteor.publishComposite('users.profile', function(_id, limit) {
   }
 });
 
-Meteor.publish('users.all', function(query, limit) {
-  check(query, String);
-  check(limit, Number);
-
-  if (this.userId) {
-    if (query) {
-      Counts.publish(this, 'users.all', Meteor.users.find({ $text: { $search: query } }), { noReady: true });
-      return Meteor.users.find(
-        {
-          $text: {
-            $search: query
-          }
-        },
-        {
-          fields: {
-            score: {
-              $meta: 'textScore'
-            }
-          },
-          sort: {
-            score: {
-              $meta: 'textScore'
-            }
-          },
-          limit: limit
-        }
-      );
-    } else {
-      Counts.publish(this, 'users.all', Meteor.users.find(), { noReady: true });
-      return Meteor.users.find({}, { sort: { createdAt: -1 }, limit: limit });
-    }
-  } else {
-    return [];
-  }
-});
-
-Meteor.publish('users.following', function() {
-  if (this.userId) {
-    let currentUser = Meteor.users.findOne({ _id: this.userId });
-
-    if (currentUser.followingIds && currentUser.followingIds.length !== 0) {
-      return Meteor.users.find({ _id: { $in: currentUser.followingIds } }, { sort: { username: 1 } });
-    } else {
-      return [];
-    }
-  } else {
-    return [];
-  }
-});
-
-Meteor.publish('users.follower', function() {
-  if (this.userId) {
-    let currentUser = Meteor.users.findOne({ _id: this.userId });
-
-    return Meteor.users.find({ followingIds: { $in: [currentUser._id] } }, { sort: { username: 1 } });
-  } else {
-    return [];
-  }
-});
-
-Meteor.publish('messages.all', function() {
-  if (this.userId) {
-    let currentUser = Meteor.users.findOne({_id: this.userId});
-
-    return Messages.find({ $or: [{ originatingFromId: currentUser._id }, {originatingToId: currentUser._id }] });
-  } else {
-    return [];
-  }
-});
-
-Meteor.publish('jobs.all', function(query, limit) {
-  check(query, String);
-  check(limit, Number);
-
-  if (this.userId) {
-    if (query) {
-      Counts.publish(this, 'jobs.all', Jobs.find({title: { $regex: '.*' + query + '.*', $options: 'i' } }), { noReady: true });
-      return Jobs.find({ title: { $regex: '.*' + query + '.*', $options: 'i' } }, { sort: { createdOn: -1 }, limit: limit });
-    } else {
-      Counts.publish(this, 'jobs.all', Jobs.find({}), { noReady: true });
-      return Jobs.find({}, { sort: { createdOn: -1 }, limit: limit });
-    }
-  } else {
-    return [];
-  }
-});
+//Meteor.publish('userStatus', function() {
+//  return Meteor.users.find({'status.online': true });
+//});
+//
+//Meteor.publishComposite('posts.all', function(query, filter, limit) {
+//  check(query, String);
+//  check(filter, String);
+//  check(limit, Number);
+//
+//  if (this.userId) {
+//    let currentUser = Meteor.users.findOne({ _id: this.userId });
+//
+//    let parameters = {
+//      find: {},
+//      options: {}
+//    };
+//
+//    if (filter === 'following') {
+//      if (currentUser.followingIds && currentUser.followingIds.length !== 0) {
+//        parameters.find.authorId = { $in: currentUser.followingIds };
+//      } else {
+//        parameters.find.authorId = { $in: [] };
+//      }
+//    }
+//
+//    return {
+//      find: () => {
+//        if (query) {
+//          parameters.find.$text = { $search: query };
+//          parameters.options = {
+//            fields: { score: { $meta: 'textScore' } },
+//            sort: { score: { $meta: 'textScore' } },
+//            limit: limit
+//          };
+//        } else {
+//          parameters.options = { sort: { createdAt: -1 }, limit: limit };
+//        }
+//        Counts.publish(this, 'posts.all', Posts.find(parameters.find), { noReady: true });
+//        return Posts.find(parameters.find, parameters.options);
+//      },
+//      children: [
+//        {
+//          find: (post) => {
+//            return Meteor.users.find({ _id: post.authorId }, { fields: { emails: 1, username: 1 } });
+//          }
+//        }
+//      ]
+//    };
+//  } else {
+//    return [];
+//  }
+//});
+//
+//Meteor.publish('users.all', function(query, limit) {
+//  check(query, String);
+//  check(limit, Number);
+//
+//  if (this.userId) {
+//    if (query) {
+//      Counts.publish(this, 'users.all', Meteor.users.find({ $text: { $search: query } }), { noReady: true });
+//      return Meteor.users.find(
+//        {
+//          $text: {
+//            $search: query
+//          }
+//        },
+//        {
+//          fields: {
+//            score: {
+//              $meta: 'textScore'
+//            }
+//          },
+//          sort: {
+//            score: {
+//              $meta: 'textScore'
+//            }
+//          },
+//          limit: limit
+//        }
+//      );
+//    } else {
+//      Counts.publish(this, 'users.all', Meteor.users.find(), { noReady: true });
+//      return Meteor.users.find({}, { sort: { createdAt: -1 }, limit: limit });
+//    }
+//  } else {
+//    return [];
+//  }
+//});
+//
+//Meteor.publish('users.following', function() {
+//  if (this.userId) {
+//    let currentUser = Meteor.users.findOne({ _id: this.userId });
+//
+//    if (currentUser.followingIds && currentUser.followingIds.length !== 0) {
+//      return Meteor.users.find({ _id: { $in: currentUser.followingIds } }, { sort: { username: 1 } });
+//    } else {
+//      return [];
+//    }
+//  } else {
+//    return [];
+//  }
+//});
+//
+//Meteor.publish('users.follower', function() {
+//  if (this.userId) {
+//    let currentUser = Meteor.users.findOne({ _id: this.userId });
+//
+//    return Meteor.users.find({ followingIds: { $in: [currentUser._id] } }, { sort: { username: 1 } });
+//  } else {
+//    return [];
+//  }
+//});
+//
+//Meteor.publish('messages.all', function() {
+//  if (this.userId) {
+//    let currentUser = Meteor.users.findOne({_id: this.userId});
+//
+//    return Messages.find({ $or: [{ originatingFromId: currentUser._id }, {originatingToId: currentUser._id }] });
+//  } else {
+//    return [];
+//  }
+//});
+//
+//Meteor.publish('jobs.all', function(query, limit) {
+//  check(query, String);
+//  check(limit, Number);
+//
+//  if (this.userId) {
+//    if (query) {
+//      Counts.publish(this, 'jobs.all', Jobs.find({title: { $regex: '.*' + query + '.*', $options: 'i' } }), { noReady: true });
+//      return Jobs.find({ title: { $regex: '.*' + query + '.*', $options: 'i' } }, { sort: { createdOn: -1 }, limit: limit });
+//    } else {
+//      Counts.publish(this, 'jobs.all', Jobs.find({}), { noReady: true });
+//      return Jobs.find({}, { sort: { createdOn: -1 }, limit: limit });
+//    }
+//  } else {
+//    return [];
+//  }
+//});
