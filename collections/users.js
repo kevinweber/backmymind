6e3dce7df687fcb302ff9f1280befa834348ee45
kevinweber <k-iwi@web.de>
2016-07-18 22:26:00 -0700

    Make first name required and email optional

--- collections/users.js
+++ collections/users.js
@@ -6,7 +6,7 @@
  */
 ValidEmail = Match.Where(function (x) {
   check(x, String);
-  return x.length <= 254 && ValidEmail.regex.test(x);
+  return x.length === 0 || (x.length <= 254 && ValidEmail.regex.test(x));
 });
 
 ValidEmail.regex = /^[A-Z0-9._%+\-]+@[A-Z0-9\.\-]+\.[A-Z]{2,}$/i;
