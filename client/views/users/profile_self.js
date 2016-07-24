5d5174dd2bf073ef9affddcee85f314b4ca06634
kevinweber <k-iwi@web.de>
2016-07-24 14:51:28 -0700

    Reset profile after submit

--- client/views/users/profile_self.js
+++ client/views/users/profile_self.js
@@ -1,3 +1,14 @@
+resetForm = ($form) => {
+  $form.find('.form-section.editable.active').each(function () {
+    let $this = $(this);
+    
+    $this.find('input, textarea').attr('readonly', true);
+    $this.removeClass('active');
+  })
+  
+  $form.find('input[type=submit]').addClass('disabled');
+}
+
 Template.self.events({
   'submit [data-id=update-profile-form]': (event, template) => {
     event.preventDefault();
@@ -16,6 +27,8 @@ Template.self.events({
         Bert.alert(error.reason, 'danger', 'growl-top-right');
       } else {
         Bert.alert('Profile successfully updated', 'success', 'growl-top-right');
+        
+        resetForm($('[data-id=update-profile-form]'));
       }
     });
   }
