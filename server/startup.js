c466e5ee06c49ac1ae92a3a931d9a7657104c94f
kevinweber <k-iwi@web.de>
2016-07-24 16:36:16 -0700

    Remove requirement to have a username

--- server/startup.js
+++ server/startup.js
@@ -3,30 +3,22 @@ Meteor.startup(() => {
     if (user.emails && user.emails[0].address.length !== 0) {
       return true;
     }
-    throw new Meteor.Error(403, 'E-Mail address should not be blank');
-  });
-  Accounts.validateNewUser((user) => {
-    if (user.username && user.username.length >= 3) {
-      return true;
-    }
-    throw new Meteor.Error(403, 'Username must have at least 3 characters');
+    throw new Meteor.Error(403, 'E-Mail address must not be blank');
   });
 
-//  ServiceConfiguration.configurations.upsert({
-//    service: "facebook"
-//  }, {
-//    $set: {
-//      appId: Meteor.settings.private.facebook.appId,
-//      secret: Meteor.settings.private.facebook.secret,
-//      requestPermissions: ["user_friends"]
-//    }
-//  });
+  //  ServiceConfiguration.configurations.upsert({
+  //    service: "facebook"
+  //  }, {
+  //    $set: {
+  //      appId: Meteor.settings.private.facebook.appId,
+  //      secret: Meteor.settings.private.facebook.secret,
+  //      requestPermissions: ["user_friends"]
+  //    }
+  //  });
 
-  Meteor.startup(function () {
-    Meteor.Sendgrid.config({
-      username: Meteor.settings.private.sendgrid.username,
-      password: Meteor.settings.private.sendgrid.password
-    });
+  Meteor.Sendgrid.config({
+    username: Meteor.settings.private.sendgrid.username,
+    password: Meteor.settings.private.sendgrid.password
   });
 
   //  Meteor.Sendgrid.send({
